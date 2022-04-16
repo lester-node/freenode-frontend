24526b54a8b3b2d95a8c11ad47befea41edff700
Lester <1>
2022-04-17 01:24:05 +0800

    feat: 代码检验缺点

--- config/config.js
+++ config/config.js
@@ -13,5 +13,45 @@ export default defineConfig({
   targets: {
     ie: 11,
   },
-  publicPath:'./'
+  publicPath: './',
+  extraBabelPlugins: [
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }, 'antd'],
+    ...(process.env.NODE_ENV == 'development' ? [] : ['transform-remove-console']),
+  ],
+  ...(process.env.NODE_ENV == 'development'
+    ? {}
+    : {
+      chunks: ['antdesigns', 'vendors', 'umi'],
+      chainWebpack: function (config, { webpack }) {
+        config.merge({
+          optimization: {
+            minimize: true,
+            splitChunks: {
+              chunks: 'async',
+              minSize: 30000,
+              maxSize: 0,
+              minChunks: 1,
+              maxAsyncRequests: 5,
+              maxInitialRequests: 3,
+              automaticNameDelimiter: '~',
+              name: true,
+              cacheGroups: {
+                vendors: {
+                  name: 'vendors',
+                  chunks: 'all',
+                  test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom|lodash|lodash-decorators|redux-saga|re-select|dva|moment)[\\/]/,
+                  priority: -10,
+                },
+                antdesigns: {
+                  name: 'antdesigns',
+                  chunks: 'all',
+                  test: /[\\/]node_modules[\\/](@ant-design|antd)[\\/]/,
+                  priority: -11,
+                },
+              },
+            },
+          },
+        });
+      },
+    }),
 });
